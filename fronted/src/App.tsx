5e0580f98f892565568336ea84b3924e025fccf2
Muhtarov Nurdin <nurdinmuhtarovv@gmail.com>
2024-10-25 16:36:14 +0600

    init app

---
+++ fronted/src/App.tsx
@@ -0,0 +1,23 @@
+import Header from "@/components/Header/Header.tsx";
+import {Button} from "@/components/ui/button.tsx";
+
+const App = () => (
+    <>
+        <header>
+            <Header/>
+        </header>
+        <main>
+            <h1 className="text-3xl font-bold underline">
+                Hello world!
+            </h1>
+            <div>
+                <Button>Click me</Button>
+            </div>
+        </main>
+        <footer>
+
+        </footer>
+    </>
+);
+
+export default App
